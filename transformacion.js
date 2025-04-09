3c505545dca5803fe3c8f3e64e883e74f5fed413
abdsam <abdsam@alu.edu.gva.es>
2025-04-09 20:17:56 +0200

    Guardar

--- transformacion.js
+++ transformacion.js
@@ -1,76 +1,46 @@
 const fs = require('fs');
 const Handlebars = require('handlebars');
 
-// Registrar helpers personalizados de manera compatible
-Handlebars.registerHelper('eq', function(a, b, options) {
-    if (a === b) {
-        return options.fn(this);
-    }
-    return options.inverse ? options.inverse(this) : '';
+// Helpers solicitados
+
+// Comparación de igualdad
+Handlebars.registerHelper('eq', function (a, b) {
+  return a === b;
 });
 
-Handlebars.registerHelper('or', function(a, b, options) {
-    if (a || b) {
-        return options.fn(this);
-    }
-    return options.inverse ? options.inverse(this) : '';
+// Operador OR lógico
+Handlebars.registerHelper('or', function (a, b) {
+  return a || b;
 });
 
-Handlebars.registerHelper('lt', function(a, b, options) {
-    if (a < b) {
-        return options.fn(this);
-    }
-    return options.inverse ? options.inverse(this) : '';
+// Suma de habilidades (asume que recibe un array de números o propiedades numéricas)
+Handlebars.registerHelper('SumarHabilidades', function (habilidades) {
+  if (!Array.isArray(habilidades)) return 0;
+  return habilidades.reduce((suma, h) => suma + (typeof h === 'number' ? h : 0), 0);
 });
 
-Handlebars.registerHelper('SumarHabilidades', function(habilidades) {
-    return habilidades.reduce((sum, h) => sum + h.valor, 0);
+// Acceder a propiedades dinámicamente
+Handlebars.registerHelper('get', function (obj, key) {
+  if (obj && obj[key] !== undefined) {
+    return obj[key];
+  }
+  return '';
 });
 
-Handlebars.registerHelper('getHabilidad', function(habilidades, cod) {
-    const hab = habilidades.find(h => h.cod === cod);
-    return hab ? hab.valor : 0;
-});
-
-Handlebars.registerHelper('sortByFuerza', function(jugadores) {
-    return [...jugadores].sort((a, b) => {
-        const fuerzaA = a.habilidades.find(h => h.cod === 'FUE').valor;
-        const fuerzaB = b.habilidades.find(h => h.cod === 'FUE').valor;
-        return fuerzaB - fuerzaA;
-    });
-});
-
-Handlebars.registerHelper('sortByNivel', function(jugadores) {
-    return [...jugadores].sort((a, b) => b.nivel - a.nivel);
-});
-
-Handlebars.registerHelper('lookup', function(obj, key) {
-    return obj && obj[key];
-});
-
-// Leer el archivo JSON
+// Leer archivo JSON con los datos del clan
 const jsonData = JSON.parse(fs.readFileSync('Clan3.json', 'utf8'));
 
-// Preparar datos para la plantilla
-const data = {
-    ...jsonData,
-    jugadores: jsonData.jugadores.map(j => j.jugador),
-    jugadoresById: jsonData.jugadores.reduce((acc, j) => {
-        acc[j.jugador.id] = j.jugador;
-        return acc;
-    }, {})
-};
+// Leer la plantilla Handlebars (.hbr)
+const plantilla = fs.readFileSync('platillaClan.hbr', 'utf8');
 
-// Leer la plantilla Handlebars
-const templateSource = fs.readFileSync('platillaClan.hbr', 'utf8');
+// Compilar plantilla
+const template = Handlebars.compile(plantilla);
 
-// Compilar la plantilla
-const template = Handlebars.compile(templateSource);
+// Generar HTML con los datos del JSON
+const htmlGenerado = template(jsonData);
 
-// Generar el HTML
-const htmlGenerado = template(data);
-
-// Guardar el resultado
+// Guardar el resultado en un archivo HTML
 fs.writeFileSync('resultado.html', htmlGenerado, 'utf8');
 
+// Mensaje de confirmación
 console.log('HTML generado con éxito: resultado.html');
